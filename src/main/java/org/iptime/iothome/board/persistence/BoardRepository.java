99a7c5f136b424ece81e144e3e5b0446b72d42af
jw <jwlee1728@gmail.com>
2019-03-28 07:11:48 +0900

    게시판 테스트 코드 작

--- src/main/java/org/iptime/iothome/board/persistence/BoardRepository.java
+++ src/main/java/org/iptime/iothome/board/persistence/BoardRepository.java
@@ -1,8 +1,40 @@
 package org.iptime.iothome.board.persistence;
 
+import java.util.List;
+
 import org.iptime.iothome.board.domain.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface BoardRepository extends CrudRepository<Board, Long>{
-
+    
+    //query method : 리턴 타입은 Page<T>, Slice<T>, List<T>
+    /*
+     * find <Entitly> By <Column> 조건 Or/And <Column> 조건 OrderBy<Column>Desc/Asc 
+     * 
+     * find..By..
+     * read..By..
+     * query..By..
+     * get..By..
+     * count..By
+     */
+    
+    //제목 equal query method
+    public List<Board> findByTitle(String title);
+    
+    //게시글 번호 정렬 페이징
+    public List<Board> findByBnoGreaterThanOrderByBnoDesc(long bno, Pageable pageable);
+    
+    //게시글 페이징 조회(page<T>)
+    public Page<Board> findByBnoGreaterThan(Long bno, Pageable pageable);
+    
+    //@Query annotation 
+    // JPQL or native SQL 
+    
+    //제목 equals Query annotation
+    //의도한대로 실행계획이 수립되지 않을 때
+    @Query("SELECT b FROM Board b WHERE b.title LIKE %?1% AND b.bno > 0 ORDER BY b.bno DESC")
+    public List<Board> findByTitleQueryA(String title);
 }
--- src/test/java/org/iptime/iothome/board/persistence/BoardRepositoryTest.java
+++ src/test/java/org/iptime/iothome/board/persistence/BoardRepositoryTest.java
@@ -2,11 +2,18 @@ package org.iptime.iothome.board.persistence;
 
 import static org.junit.Assert.*;
 
+import java.util.Collection;
+import java.util.List;
+
 import org.iptime.iothome.board.domain.Board;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -91,5 +98,71 @@ public class BoardRepositoryTest {
         /***then***/
         Board deletedBoard = boardRepository.findOne(id);
         assertNull(deletedBoard);
+    }
+    
+    //board make dummy data
+    @Test
+    public void insertBoard200DataTest() {
+        for(int i = 0; i < 200; i++) {
+            Board board = new Board();
+            board.setTitle("제목.." + i);
+            board.setContent("내용 ...." + i + " 채우기");
+            board.setWriter("user0" + (i%10));
+            boardRepository.save(board);
+        }
+    }
+    
+    //query method 테스트
+    @Test
+    public void queryMethodByTitleTest() {
+        /***given***/
+        
+        /***when***/
+        List<Board> boardList = boardRepository.findByTitle("제목..177");
+        
+        /***then***/
+        assertEquals(1, boardList.size());
+        assertEquals("내용 ....177 채우기", boardList.get(0).getContent());
+    }
+    
+    //게시글 번호 정렬 페이징
+    @Test
+    public void findByBnoGreaterThanOrderByBnoDescTest() {
+        /***given***/
+        Pageable pageable = new PageRequest(1, 10);
+
+        /***when***/
+        Collection<Board> boardList = boardRepository.findByBnoGreaterThanOrderByBnoDesc(0L, pageable); 
+
+        /***then***/
+        assertEquals(10, boardList.size());
+    }
+    
+    //게시글 번호 페이징 조회(Page<T>)
+    @Test
+    public void findByBnoGreaterTest() {
+        /***given***/
+        Pageable pageable = new PageRequest(0, 10, Sort.Direction.ASC, "bno");
+        
+        /***when***/
+        Page<Board> result = boardRepository.findByBnoGreaterThan(0l, pageable);
+        
+        List<Board> boardList = result.getContent();
+        
+        /***then***/
+        assertEquals(10, boardList.size());
+        assertEquals(0, result.getNumber());
+    }
+    
+    //게시글 제목으로 조회  Query annotationgit clone https://github.com/spring-guides/gs-rest-service-cors.git
+    @Test
+    public void findByTitleQueryATest() {
+        /***given***/
+        
+        /***when***/
+        List<Board> boardList = boardRepository.findByTitleQueryA("제목..177");
+                
+        /***then***/
+        assertEquals(1, boardList.size());
     }
 }
